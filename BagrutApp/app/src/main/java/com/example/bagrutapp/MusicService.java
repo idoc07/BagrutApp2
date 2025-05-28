5c83004aec361e429d0ee7ba35818ae2a2805eaa
idoc07 <108226408+idoc07@users.noreply.github.com>
2025-05-28 14:19:58 +0300

    adding music

---
+++ BagrutApp/app/src/main/java/com/example/bagrutapp/MusicService.java
@@ -0,0 +1,39 @@
+package com.example.bagrutapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MusicService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.background);
+        mediaPlayer.setLooping(true); // ממשיך לנגן בלופ
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
+        return START_STICKY; // ממשיך לרוץ עד שתעצור אותו ידנית
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
