a8a11172a3c1bfef3092b6a921b014a3ebf30ff6
idoc07 <108226408+idoc07@users.noreply.github.com>
2025-05-28 14:44:42 +0300

    Adding BaseActivity

---
+++ BagrutApp/app/src/main/java/com/example/bagrutapp/BaseActivity.java
@@ -0,0 +1,34 @@
+// BaseActivity.java
+package com.example.bagrutapp;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected boolean isMusicPlaying = false;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_toggle_music) {
+            if (isMusicPlaying) {
+                stopService(new Intent(this, MusicService.class));
+                item.setIcon(R.drawable.baseline_volume_off_24);
+            } else {
+                startService(new Intent(this, MusicService.class));
+                item.setIcon(R.drawable.baseline_volume_up_24);
+            }
+            isMusicPlaying = !isMusicPlaying;
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
