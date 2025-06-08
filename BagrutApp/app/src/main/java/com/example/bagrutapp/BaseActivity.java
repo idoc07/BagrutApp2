4c956786e9d6f065381afa61e1bbd1c338dfdf7c
idoc07 <108226408+idoc07@users.noreply.github.com>
2025-06-08 13:11:58 +0300

    improving the shared toolbar

--- BagrutApp/app/src/main/java/com/example/bagrutapp/BaseActivity.java
+++ BagrutApp/app/src/main/java/com/example/bagrutapp/BaseActivity.java
@@ -1,7 +1,10 @@
 // BaseActivity.java
 package com.example.bagrutapp;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +14,19 @@ public class BaseActivity extends AppCompatActivity {
     protected boolean isMusicPlaying = false;
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
 
-        // קובעים אייקון התחלתי בהתאם ל־isMusicPlaying
+        // בדיקה בזמן אמת אם ה-Service רץ
+        isMusicPlaying = isMusicServiceRunning();
+
         MenuItem musicItem = menu.findItem(R.id.action_toggle_music);
-        if (isMusicPlaying) {
-            musicItem.setIcon(R.drawable.baseline_volume_up_24);
-        } else {
-            musicItem.setIcon(R.drawable.baseline_volume_off_24);
-        }
+        updateMusicIcon(musicItem);
 
         return true;
     }
@@ -30,14 +36,31 @@ public class BaseActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.action_toggle_music) {
             if (isMusicPlaying) {
                 stopService(new Intent(this, MusicService.class));
-                item.setIcon(R.drawable.baseline_volume_off_24);
             } else {
                 startService(new Intent(this, MusicService.class));
-                item.setIcon(R.drawable.baseline_volume_up_24);
             }
             isMusicPlaying = !isMusicPlaying;
+            updateMusicIcon(item);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateMusicIcon(MenuItem item) {
+        if (isMusicPlaying) {
+            item.setIcon(R.drawable.baseline_volume_up_24);
+        } else {
+            item.setIcon(R.drawable.baseline_volume_off_24);
+        }
+    }
+
+    private boolean isMusicServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (MusicService.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
