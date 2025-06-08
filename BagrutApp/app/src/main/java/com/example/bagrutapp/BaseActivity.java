f3105afbcc0e5f5583db8bc4a2939651c33fbad9
idoc07 <108226408+idoc07@users.noreply.github.com>
2025-06-08 11:15:08 +0300

    toggle music improvement

--- BagrutApp/app/src/main/java/com/example/bagrutapp/BaseActivity.java
+++ BagrutApp/app/src/main/java/com/example/bagrutapp/BaseActivity.java
@@ -13,6 +13,15 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        // קובעים אייקון התחלתי בהתאם ל־isMusicPlaying
+        MenuItem musicItem = menu.findItem(R.id.action_toggle_music);
+        if (isMusicPlaying) {
+            musicItem.setIcon(R.drawable.baseline_volume_up_24);
+        } else {
+            musicItem.setIcon(R.drawable.baseline_volume_off_24);
+        }
+
         return true;
     }
 
